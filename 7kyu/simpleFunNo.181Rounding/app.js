07651a92ef27dce41839e596af9a7b64af6f4616
josh.bello <josh.bello@thirtythree.co.uk>
2024-03-26 11:25:27 +0000

    added (7kyu/simpleFunNo.181Rounding/) completed js and php keta

---
+++ 7kyu/simpleFunNo.181Rounding/app.js
@@ -0,0 +1,15 @@
+function rounding(n, m) {
+    //coding and coding..
+    return (Math.round(n / m)) * n;
+    
+  }
+
+  console.log(rounding(20,3));//,21)
+
+console.log(rounding(19,3));//,18)
+
+console.log(rounding(1,10));//,0)
+
+console.log(rounding(50,100));//,50)
+
+console.log(rounding(123,456));//,0)
