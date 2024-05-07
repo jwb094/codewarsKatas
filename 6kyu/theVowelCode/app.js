7911490f77e41bdbdf4cfaf732ca4b6b3055e962
Josh Bello <josh.bello@thirtythree.co.uk>
2024-05-07 16:02:12 +0100

    add(6kyu - theVowelCode) theVowelCode  completed

---
+++ 6kyu/theVowelCode/app.js
@@ -0,0 +1,69 @@
+    // An array of keys
+    var keys = [1, 2, 3,4,5];
+     
+    // An array of values
+    var values = ['a', 'e', "i", "o", "u"];
+     
+    // Object created
+    var obj = {};
+     
+    // Using loop to insert key
+    // value in Object
+    for(var i = 0; i < keys.length; i++){
+        obj[keys[i]] = values[i];
+    }
+     
+    // Printing object
+    // for (var key of Object.keys(obj)) {
+    //     document.write(key + " => " + obj[key] + "</br>")
+    // }
+
+    //console.log(values);
+function encode(string) {
+    
+
+    
+let letters =string.split('');
+
+for (let index = 0; index <= letters.length; index++) {
+    const element = letters[index];
+  //  if( values.includes(element)){
+
+        let indexSelected;
+        // for (let lettersIndex = 0; lettersIndex <= letters.length; lettersIndex++) {
+        
+            for (var key of Object.keys(obj)) {
+                if (obj[key] === element) {
+                    console.log(obj[key]);
+                    letters[index] = key.toString();
+                }
+                //console.log(indexSelected);
+            }
+
+       // }
+    } 
+
+//console.log(letters);
+    return letters.toString().replace(/,/g, '');
+  }
+  
+function decode(string) {
+
+    let letters =string.split('');
+    for (let index = 0; index < letters.length; index++) {
+        for (var key of Object.keys(obj)) {
+            if (letters[index] === key) {
+                letters[index] = obj[key].toString();
+            }
+       
+        }
+    }
+    return letters.toString().replace(/,/g, '');
+  }
+
+
+
+  console.log(encode('hello'));//, 'h2ll4');
+  console.log(encode('How are you today?'));//, 'H4w 1r2 y45 t4d1y?');
+  console.log(encode('This is an encoding test.'));//, 'Th3s 3s 1n 2nc4d3ng t2st.');
+  console.log(decode('h2ll4'));//, 'hello');
