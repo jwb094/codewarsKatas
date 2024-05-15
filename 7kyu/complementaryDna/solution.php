4ed7f2439a41d9ef0c56432c3eed4452d4535a54
Josh Bello <josh.bello@thirtythree.co.uk>
2024-05-15 16:40:11 +0100

    add (7kyu/complementaryDna) new kata completed in php-2

--- 7kyu/complementaryDna/solution.php
+++ 7kyu/complementaryDna/solution.php
@@ -1,6 +1,7 @@
 <?php
 
-function DNA_strand($dna) {
+function DNA_strand($dna)
+{
     // Your code here
 
     $output = "";
@@ -14,15 +15,15 @@ function DNA_strand($dna) {
 
 
     foreach (str_split($dna) as $dna_key => $dna_value) {
-        if (array_key_exists($dna_value,$complementaryDNA)){
-          $output .= $complementaryDNA[$dna_value];
+        if (array_key_exists($dna_value, $complementaryDNA)) {
+            $output .= $complementaryDNA[$dna_value];
         }
-        if (in_array($dna_value,$complementaryDNA)){
+        if (in_array($dna_value, $complementaryDNA)) {
             $output .=  $key = array_search($dna_value, $complementaryDNA, true);
         }
     }
-return $output;
-  }
+    return $output;
+}
 
 
 
@@ -30,9 +31,9 @@ return $output;
 
 
 
-  var_dump(DNA_strand("AAAA"));//TTTT
-  var_dump(DNA_strand("TTTT"));//AAAA
-  var_dump(DNA_strand("ATTGC"));//"TAACG"
-  var_dump(DNA_strand("TAACG"));//"ATTGC"
-  var_dump(DNA_strand("GTAT"));//"ATTGC"
-  var_dump(DNA_strand("CATA"));//"GTAT"
+var_dump(DNA_strand("AAAA")); //TTTT
+var_dump(DNA_strand("TTTT")); //AAAA
+var_dump(DNA_strand("ATTGC")); //"TAACG"
+var_dump(DNA_strand("TAACG")); //"ATTGC"
+var_dump(DNA_strand("GTAT")); //"ATTGC"
+var_dump(DNA_strand("CATA"));//"GTAT"
