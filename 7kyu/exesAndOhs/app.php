2cb68adc3d188597eca8f21c4412a2f1263055a5
Josh Bello <josh.bello@thirtythree.co.uk>
2024-06-04 17:00:37 +0100

    add(7kyu - exesAndOhs) completed kyu in php

---
+++ 7kyu/exesAndOhs/app.php
@@ -0,0 +1,22 @@
+<?php
+
+function XO($s) {
+    $x = substr_count(strtolower($s), 'x');
+    $o = substr_count(strtolower($s), 'o');
+   // print_r($x);
+    //print_r($o);
+
+    if ($x === 0 && $o === 0) {
+        return true;
+    }
+    return($x === $o) ? true : false ;
+    
+
+  }
+
+
+var_dump(XO('xo'));//true,
+var_dump(XO('XO'));//true,
+var_dump(XO('xo0'));//true,
+var_dump(XO('xxxoo'));//false
+var_dump(XO("xxOo"));//true,
