c5224a563017766f4cc49f392867b9329e95e359
Josh Bello <josh.bello@thirtythree.co.uk>
2024-07-01 11:21:16 +0100

    added (7kyu/spacify) completed in php

---
+++ 7kyu/spacify/app.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function spacify(string $s) {
+    // Your code here
+
+
+//    var_dump(implode(" ",str_split($s,1)));
+return implode(" ",str_split($s,1)); 
+}
+
+
+
+  var_dump(spacify("hello world"));
