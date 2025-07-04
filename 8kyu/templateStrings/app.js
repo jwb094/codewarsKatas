31c031137d75f15eb8e6b91fdbfb34df115295cd
Josh Bello <Jb230913@live.com>
2025-07-04 10:11:59 +0100

    add  - completed template Strings

---
+++ 8kyu/templateStrings/app.js
@@ -0,0 +1,9 @@
+var templateStrings = function(noun, adjective) {
+  return ` ${noun} are ${adjective}`;
+}
+
+
+
+console.log(templateStrings('Animals', 'Good'));
+console.log(templateStrings('You', 'special'));
+console.log(templateStrings('lives', 'frozen'));
