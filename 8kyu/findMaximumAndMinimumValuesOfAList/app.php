25cf76a99935cda16a2a5dd430b8c7edc6c178f5
Josh Bello <Jb230913@live.com>
2025-07-05 16:49:32 +0100

    added - comppleted 8kyu/findMaximumAndMinimumValuesOfAList/ - in php

---
+++ 8kyu/findMaximumAndMinimumValuesOfAList/app.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+function maximum($array) {
+  // Your code here
+  return max($array);
+}
+function minimum($array) {
+  // Your code here
+  return min($array);
+}
+
+echo maximum(array(1, 2, 3, 4, 5));
+echo minimum(array(1, 2, 3, 4, 5));
