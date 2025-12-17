5ca29430a4a2af45b043066de59db263316d5f8f
Josh Bello <Jb230913@live.com>
2025-12-17 18:05:52 +0000

    add - completed switchItUp kyu in php

---
+++ 8kyu/switchItUp/app.php
@@ -0,0 +1,42 @@
+<?php
+
+function switch_it_up($number)
+{
+
+    switch ($number) {
+        case 0:
+            return  "Zero";
+        case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+
+        case 4:
+            return "Four";
+
+        case 5:
+            return "Five";
+
+        case 6:
+            return "Six";
+
+        case 7:
+            return "Seven";
+
+        case 8:
+            return "Eight";
+
+        case 9:
+            return "nINE";
+
+        default:
+            return "Its out of range";
+    }
+}
+
+
+print_r(switch_it_up(1)); //"One"
+print_r(switch_it_up(3)); //Three
+print_r(switch_it_up(5));//Five
