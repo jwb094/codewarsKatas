d1ffa4f22c306c8052c17e5934145dd42b78b75d
Josh Bello <Jb230913@live.com>
2025-12-18 17:28:01 +0000

    add - completed Volume of a cuboid

---
+++ 8kyu/VolumeofaCuboid/app.php
@@ -0,0 +1,13 @@
+<?php
+
+function get_volume_of_cuboid($length, $width, $height)
+{
+    // Your code here
+
+    return $length * $width * $height;
+}
+
+
+
+print_r(get_volume_of_cuboid(1, 2, 2));
+print_r(get_volume_of_cuboid(6, 2, 5));
