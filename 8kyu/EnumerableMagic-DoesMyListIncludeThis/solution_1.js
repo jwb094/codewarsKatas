a9eae9146596cef9881c0d555b2d77fded25035c
Josh Bello <Jb230913@live.com>
2026-06-24 09:39:57 +0100

    add - EnumerableMagic-DoesMyListIncludeThis

---
+++ 8kyu/EnumerableMagic-DoesMyListIncludeThis/solution_1.js
@@ -0,0 +1,8 @@
+function include(arr, item) {
+  // ...
+  return arr.includes(item);
+}
+
+console.log(include([1, 2, 3, 4], 3)); //true
+console.log(include([1, 2, 4, 5], 3)); //false
+console.log(include([], 3)); //false
