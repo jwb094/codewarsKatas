e85e46370feb1c6071ad8126a6758bb52a0f9396
Josh Bello <Jb230913@live.com>
2026-08-14 21:36:53 +0100

    add - completed php version of kata

---
+++ 8kyu/Drink About/app.php
@@ -0,0 +1,20 @@
+<?
+
+function people_with_age_drink(int $old): string {
+  // Your code here
+if($old < 14){
+return 'drink toddy';
+}
+if($old >= 14 && $old < 18){
+return 'drink coke';
+}
+
+if($old >= 18 && $old < 21){
+return 'drink beer';
+}
+
+if($old >= 21){
+return 'drink whisky';
+}
+
+}
