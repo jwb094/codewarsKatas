5dc0b569a6085264a6c0d2f460a7652afe23fd59
Josh Bello <Jb230913@live.com>
2026-08-20 19:34:15 +0100

    add - completed Basicmathematicaloperations in php

---
+++ 8kyu/Basicmathematicaloperations/app.php
@@ -0,0 +1,17 @@
+<?php
+function basicOp($op, $val1, $val2)
+{   
+
+    if($op === '+'){
+        return  $val1 +  $val2;
+    }
+    if($op === '-'){
+        return  $val1 -  $val2;
+    }
+    if($op === '*'){
+        return  $val1 *  $val2;
+    }
+    if($op === '/'){
+        return  $val1 /  $val2;
+    }
+ }
