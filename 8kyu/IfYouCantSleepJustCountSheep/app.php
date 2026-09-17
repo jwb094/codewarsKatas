2ca84d4d7060031ccdc400d6872e1bdb9cd8245b
Josh Bello <Jb230913@live.com>
2026-09-17 19:22:00 +0100

    add - completed 8kyu IfYouCantSleepJustCountSheep in php

---
+++ 8kyu/IfYouCantSleepJustCountSheep/app.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". 
+Input will always be valid, i.e. no negative integers.
+*/
+function countsheep(int $num):string
+{
+    $answerString = "";
+
+    if ($num === 0) {
+        return "";
+    }
+
+    for ($index = 1; $index <= $num; $index++) {
+        $answerString .= "{$index} sheep...";
+    }
+
+    return $answerString;
+}
