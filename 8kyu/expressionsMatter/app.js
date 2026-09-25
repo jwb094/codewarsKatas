712b1a90514cb0a3d988142a7ca567b95c1f669a
Josh Bello <Jb230913@live.com>
2026-09-25 21:07:54 +0100

    add - completed 8kyu/expressionsMatter in Javascript

--- 8kyu/expressionsMatter/app.js
+++ 8kyu/expressionsMatter/app.js
@@ -3,27 +3,35 @@ Given three integers a, b, and c, return the largest number obtained after inser
  */
 
 function expressionMatter(a, b, c) {
-  
-    const first = a + b + c;
+  const first = a + b + c;
   const second = a + b * c;
   const third = a * b + c;
   const fourth = a * b * c;
   const fifth = a + b + c;
-  const sixth =  a + (b + c);
+  const sixth = a + (b + c);
   const seventh = (a + b) * c;
-  const eighth =  a * (b + c);
-  const nineth = (a * b) + c;
-  const tenth =  a * (b + c);
-  const eleventh =  (a * b) * c;
+  const eighth = a * (b + c);
+  const nineth = a * b + c;
+  const tenth = a * (b + c);
+  const eleventh = a * b * c;
   const twevleth = a * (b * c);
-  
-  
 
-
-  let result = [first,second,third,fourth,fifth,sixth,
-            seventh,eighth,nineth,tenth,eleventh,twevleth].sort((a, b) => {
+  let result = [
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth,
+    seventh,
+    eighth,
+    nineth,
+    tenth,
+    eleventh,
+    twevleth,
+  ].sort((a, b) => {
     return b - a;
   });
-  
-  return  result[0]; // highest achievable result
+
+  return result[0]; // highest achievable result
 }
